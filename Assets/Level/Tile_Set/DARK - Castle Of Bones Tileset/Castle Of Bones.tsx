90fbe593cb7fd37ce21f63ad7f1588cb9dd5f11e
J-1205 <iampkg1234@gmail.com>
2025-06-06 01:27:30 +0900

    TileMap_Ground_Add

---
+++ Assets/Level/Tile_Set/DARK - Castle Of Bones Tileset/Castle Of Bones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Castle Of Bones" tilewidth="16" tileheight="16" tilecount="1120" columns="40">
+ <image source="Tileset1.png" width="640" height="448"/>
+</tileset>
